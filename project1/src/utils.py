62335021cf4b0e366301728907cfd71cc6d8b926
Claudio5 <claudio.loureiro1@gmail.com>
2018-10-24 19:59:02 +0200

    add test cross validation + utils file which contains the functions

--- project1/src/utils.py
+++ project1/src/utils.py
@@ -28,14 +28,34 @@ def get_split_indexes(x, y, k_fold, seed=1):
 
     return index_split_te, index_split_tr
 
-def cross_validation(gradient_func, loss_func, tx, y, indexes_te, indexes_tr, k_fold):
+def cross_validation(optim_method, loss_func, tx, y, indexes_te, indexes_tr,
+                    k_fold, *args_optim, *args_loss):
     for i in range(k_fold):
         x_te = x[indexes_te[i]]
         y_te = y[indexes_te[i]]
         x_tr = x[indexes_tr[i]]
         y_tr = y[indexes_tr[i]]
-        
 
+        x_tr = standardize(x_tr)
+
+        w, err_tr = optim_method(y_tr, x_tr, *args_optim)
+
+        err_te = loss_function(*args_loss)
+
+        err_tr_list.append(err_tr)
+        err_te_list.append(err_re)
+
+    mse_tr_mean = np.mean(err_tr_list)
+    mse_te_mean = np.mean(err_te_list)
+
+    return mse_tr_mean, mse_te_mean
+
+
+def standardize():
+    centered_data = x - np.mean(x, axis=0)
+    std_data = centered_data / np.std(centered_data, axis=0)
+
+    return std_data
 
 x = np.array([[1,2],[1,3],[1,4],[1,9],[2,8],[1,90]])
 y = np.array([1,4,3,8,6,6])
